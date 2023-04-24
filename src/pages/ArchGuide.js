f83a13790c6491522ddaeed676fc9160a414c095
Dyna-ALISMAIL <127844372+Dyna-ALISMAIL@users.noreply.github.com>
2023-04-24 19:00:44 +0100

    DYNA
    DYNA

--- src/pages/ArchGuide.js
+++ src/pages/ArchGuide.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import './Guide.css';
-import { ReactComponent as HomeGuide} from '../Images/homeguide.svg';
 
 
 function Guide () {
  return (
   <div className="hero"> 
    <Navbar label="Guide"/>
-   <HomeGuide className='guide-image'/>
-   <a href="/guide/architecture" className="suiv">Suivant</a>
+   <a href="./instruction-format" className="suiv">Suivant</a>
+   <a href="." className="prev">Retour</a>
   </div>
  );
 };
