da297d498ddebb60b2acc5bd270c75818b986652
REZZOUGAICHA <la_rezzoug@esi.dz>
2023-04-24 21:24:29 +0100

    update

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -2,11 +2,9 @@ import React from 'react';
 import '../App.css';
 import Form from "../ComponentsSettings/Form";
 import "./Settings.css";
-import Settingsbar from '../ComponentsSettings/Settingsbar';
-import ProfilePage from '../ComponentsSettings/ProfilePage';
-import ModePage from '../ComponentsSettings/ModePage';
+
 import MotDePassePage from '../ComponentsSettings/MotDePassePage';
-import LanguePage from '../ComponentsSettings/LanguePage';
+
 import Navbar from '../components/Navbar';
 import "../ComponentsSettings/MotDePassePage.css" ; 
 
