2cc4c209fd99e4caaff7b4a80c817f2a31110b07
lylia5 <ll_aouinine@esi.dz>
2023-04-30 19:08:51 +0100

    lylia

--- General/Frontend/src/pages/Code.js
+++ General/Frontend/src/pages/Code.js
@@ -9,6 +9,7 @@ import Navbar from '../components/Navbar';
 import Button from '../components/Buttonn'
 import Side from '../components/side'
 import next from '../Images/next.svg'
+import { Link } from 'react-router-dom';
 const ButoStyle={
   background: '#00A6FB',
 position:'absolute',
@@ -120,12 +121,14 @@ function Code() {
       
       <div className="container2">    
       <Button onClick={handleClick} id="btn1" text="Compiler" style={{ fontSize: '16px', background: '#F8F9FA', color: '#023047',border:'1px solid #00A6FB',padding: '12px 24px'}}></Button>
+      <Link to="/code/Simulation">
       <div id="btn2" >
+        
         Simuler
         <svg width="9" height="16" viewBox="0 0 9 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M0.454505 1.2045C0.893845 0.765165 1.60616 0.765165 2.0455 1.2045L8.0455 7.2045C8.48483 7.64384 8.48483 8.35616 8.0455 8.7955L2.0455 14.7955C1.60616 15.2348 0.893845 15.2348 0.454505 14.7955C0.015165 14.3562 0.015165 13.6438 0.454505 13.2045L5.65901 8L0.454505 2.7955C0.015165 2.35616 0.015165 1.64384 0.454505 1.2045Z" fill="#F8F9FA"/>
 </svg>
-      </div>
+      </div></Link>
       </div>
           <div className='compiled'>
           </div>
