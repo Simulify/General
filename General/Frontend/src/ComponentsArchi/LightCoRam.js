8934931d71dcdc6af3ac1fa262898dede9cdedb7
Amina <la_malek@esi.dz>
2023-05-01 16:33:53 +0100

    Amina

--- General/Frontend/src/ComponentsArchi/LightCoRam.js
+++ General/Frontend/src/ComponentsArchi/LightCoRam.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './Light.css';
 import { MyFun } from './yellow';
-import { RimRam } from './yellow';
+import { CoRam } from './yellow';
 
-function LightRimRam() {
+function LightCoRam() {
   const [coor, setCoor] = useState([]);
   const myRef = useRef(null);
-  RimRam(myRef,setCoor,coor)
+  CoRam(myRef,setCoor,coor)
 
-  return <div className='LightRimRam' ref={myRef} />;
+  return <div className='LightCoRam' ref={myRef} />;
 }
 
-export default LightRimRam;
+export default LightCoRam;
