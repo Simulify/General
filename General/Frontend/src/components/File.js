4e5caafe0c4d3ada962ddc06ebf22846f5781c0a
Yesmean <ly_dinari@esi.dz>
2023-05-02 12:28:29 +0100

    saving is working

--- General/Frontend/src/components/File.js
+++ General/Frontend/src/components/File.js
@@ -5,15 +5,31 @@ import can from '../Images/Trash.svg';
 import { Link } from "react-router-dom";
 
 function File(props) {
+
+
+  
+  const handleClick = () => {
+    localStorage.setItem('filecodeHexa', props.codeHexa);
+    console.log(`filecodeHexa: ${props.codeHexa}`);
+    localStorage.setItem('filecodeMemo', props.codeMemo);
+    console.log(`filecodeMemo: ${props.codeMemo}`);
+    localStorage.setItem('filecompiled', props.compiled);
+    console.log(`filecompiled: ${props.compiled}`);
+    localStorage.setItem('buttonClicked', `true`);
+    console.log("buttonClicked:", localStorage.getItem("buttonClicked"));
+
+  };
   return ( 
     <div className="fileLign">
       <Link to="/code">
-        <button className="File-button" >
+        <button className="File-button" onClick={handleClick}>
           <img className="file" src={file} alt="fileicon"></img>
           {props.label}
         </button>
       </Link>
-      {props.value && <p style={{display: 'none'}}>{props.value}</p>}
+      <p style={{display: 'none'}}>{props.codeHexa}</p>
+      <p style={{display: 'none'}}>{props.codeMemo}</p>
+      {props.compiled && <p style={{display: 'none'}}>Compiled</p>}
       <img className="can" src={can} alt="deleteIcon" onClick={props.onDelete}></img>
     </div>
   );
